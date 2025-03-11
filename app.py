a438b10961c26d65227edf5333de965fe924e615
ramon <ramon.meza83@gmail.com>
2025-03-11 16:31:43 +0100

    Formateado con Black: app.py y tests/test_app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host="localhost", port=6379, db=0)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    count = r.incr('counter')
+    count = r.incr("counter")
     return f"Counter value: {count}"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 import pytest
 from app import app
 
+
 def test_home():
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert b'Counter value' in response.data
+    assert b"Counter value" in response.data
